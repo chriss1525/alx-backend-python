29b2480aef02dddae38efaf062ff5f1964715535
chriss1525 <okothchris15@gmail.com>
2023-10-06 01:35:50 +0300

    finding the floor of a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""function floor that takes a float and returns the floor of the float"""
+
+
+def floor(n: float) -> float:
+    """Return the floor of the float"""
+    return int(n)
