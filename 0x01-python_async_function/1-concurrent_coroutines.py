888df6578f2846c51fe4bb3ca43c2499188d9c7a
chriss1525 <okothchris15@gmail.com>
2024-01-02 19:33:21 +0300

    annotate arguments

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """return list of awaited responses"""
     res = await asyncio.gather(
         *tuple(map(lambda _: wait_random(max_delay), range(n)))
