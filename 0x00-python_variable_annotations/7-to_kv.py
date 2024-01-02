15dfce6a52f252ac9313328d9c50f5b2af2703fb
chriss1525 <okothchris15@gmail.com>
2024-01-02 20:34:22 +0300

    function that takes a collection of items and return a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Solution of "To Kvadrat" exercise"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """This function takes a string k and an int OR float v as arguments,"""
+    return (k, float(v**2))
