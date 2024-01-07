de899b21807e7692e9f819b0b3b4c3b1c2e27e3b
chriss1525 <okothchris15@gmail.com>
2024-01-07 21:02:38 +0300

    async function that takes no arguments

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" coroutine command that takes no arguments."""
+
+import random
+import asyncio
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """ coroutine command that takes no arguments."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
